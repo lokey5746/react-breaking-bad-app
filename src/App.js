977a0992045b82692d826344183386163051e67b
Lokendra Dangi <pachar.com@gmail.com>
2020-08-21 21:16:33 +0545

    fetch all character name

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
+import Header from "./components/ui/Header";
+import CharacterGrid from "./components/characters/CharacterGrid";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters`
+      );
+
+      console.log(result.data);
+      setItems(result.data);
+      setIsLoading(false);
+    };
+    fetchItems();
+  }, []);
+
   return (
     <div className="App">
-      <h2>Breaking Bad APP Portal</h2>
+      <Header />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 }
